ada3e069b5e43cfd15f104be79b5950c41a2c167
nguye <nguye@DESKTOP-94SQ5M7>
2018-03-25 15:13:36 +0700

    123

--- src/appium/FirstAppiumTest.java
+++ src/appium/FirstAppiumTest.java
@@ -7,26 +7,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class FirstAppiumTest {
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+	WebDriver driver;
+	DesiredCapabilities dc = new DesiredCapabilities();
 
-		DesiredCapabilities dc = new DesiredCapabilities();
+	@BeforeClass
+	public void beforeClass() {
 		dc.setCapability("BROWSER_NAME", "Android");
-		dc.setCapability("VERSION","6.0");
-		dc.setCapability("deviceName","And-1");
+		dc.setCapability("VERSION", "6.0");
+		dc.setCapability("deviceName", "And-1");
 		dc.setCapability("platformName", "Android");
-
 		dc.setCapability("appPackage", "com.android.calculator2");
 		dc.setCapability("appActivity", "com.android.calculator2.Calculator");
-		
-		WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),dc);
-		System.out.println(driver.toString());
-		driver.findElement(By.name("4")).click();
+	}
+
+	@Test
+	public void TC_01_Calculator() throws MalformedURLException {
+
+		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+		driver.findElement(By.name("1")).click();
 		driver.findElement(By.name("+")).click();
-		driver.findElement(By.name("6")).click();
+		driver.findElement(By.name("2")).click();
+		driver.findElement(By.name("+")).click();
+		driver.findElement(By.name("3")).click();
+		driver.findElement(By.name("+")).click();
+		driver.findElement(By.name("4")).click();
 		driver.findElement(By.name("=")).click();
+	}
+
+	@AfterClass
+	public void afterClass() {
 		driver.quit();
 	}
 
